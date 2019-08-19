9d035b3c6baf11d43265db47aabab233a35edf0f
jvyden420 <jvyden420@gmail.com>
2019-08-18 23:18:14 -0400

    Add automatic config reloading

--- yun.js
+++ yun.js
@@ -4,16 +4,34 @@ const client = new Discord.Client();
 const fs = require("fs");
 
 let config;
-function loadconfig() {
+function loadconfig(reloading = false) {
   const cfgloc = __dirname + "/config.json"
   const cfgfile = fs.readFileSync(cfgloc)
-  try {config = JSON.parse(cfgfile)}
+  let confignew;
+  try {
+    confignew = JSON.parse(cfgfile)
+  }
   catch(e){
     console.error(`Error while reading config ${cfgloc}: ${e}`)
-    process.exit(1)
+    if(!reloading) {process.exit(1)} else {
+      console.error(`Config will not be updated until the error is fixed.`)
+    }
+    return
   }
+  config = confignew
   console.log(`Successfully loaded config ${cfgloc}.`)
 }
+Date.prototype.unixTime = function() {
+  return Math.round(this.valueOf() / 1000)
+}
+let lastWatch = new Date(0).unixTime()
+fs.watch(__dirname + "/config.json", (event, filename) => {
+  const now = new Date().unixTime()
+  if(!filename) {return}
+  if(lastWatch === now) {return}
+  lastWatch = now
+  if(event === "change") {loadconfig(true)}
+})
 function runCommand(input, message) {
   let args = input.split(' ')
   const cmd = args[0].toLowerCase()
