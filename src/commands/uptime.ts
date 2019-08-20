ec8abf5041c47e56484b6492067e9ef430866c8c
jvyden420 <jvyden420@gmail.com>
2019-08-20 01:25:00 -0400

    Shorten uptime

--- src/commands/uptime.ts
+++ src/commands/uptime.ts
@@ -1,6 +1,5 @@
 import { Command } from "discord-akairo";
 import { Message } from "discord.js";
-
 export default class extends Command {
   constructor() {
     super("uptime", {
@@ -8,9 +7,7 @@ export default class extends Command {
       description: "Shows the uptime of the bot.",
     });
   }
-
   public async exec(message: Message) {
-    const uptime = new Date(process.uptime() * 1000).toISOString().substr(11, 8);
-    return message.channel.send(`Yun Uptime: ${uptime}`)
+    return message.channel.send("Yun Uptime: " + new Date(process.uptime() * 1000).toISOString().substr(11, 8))
   }
 }
